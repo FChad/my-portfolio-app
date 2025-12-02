27de4f35cc6678d8e85a20ec9b7e1be8c529f84b
Chad Feierstein <103460020+FChad@users.noreply.github.com>
2025-12-02 12:11:58 +0100

    Add external dependency to Vite rollup config
    Configured Vite's rollup to treat '@react-email/render' as an external dependency. This helps prevent bundling issues when using this package.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -89,6 +89,9 @@ export default defineNuxtConfig({
     experimental: {
       wasm: true
     },
+    rollupConfig: {
+      external: ['@react-email/render']
+    },
     prerender: {
       crawlLinks: true,  // Crawlt alle internen Links beim Build
       autoSubfolderIndex: false  // Verhindert automatische /index.html Generierung
